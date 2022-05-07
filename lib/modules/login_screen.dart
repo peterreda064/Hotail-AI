346c2e65e6f9f2e7ac1e09c09744e92f04bd40d4
noorolemy1 <101938171+noorolemy1@users.noreply.github.com>
2022-05-07 17:18:21 +0200

    log er

--- lib/modules/login_screen.dart
+++ lib/modules/login_screen.dart
@@ -1,5 +1,145 @@
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:hotel/modules/register_screen.dart';
+// import 'package:hotel/services/auth.dart';
+// import 'package:hotel/shared/components/compantants.dart';
+// import 'package:hotel/shared/style/colors.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:async/async.dart';
+//
+// import '../layout/home_layout.dart';
+//
+//
+// class HotelLoginScreen extends StatelessWidget {
+//   // final _authhh = FirebaseAuth.instance;
+//   // AuthBase authBase = AuthBase();
+//   var emailController = TextEditingController();
+//   var passwordController = TextEditingController();
+//   var formKey = GlobalKey<FormState>();
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: const Color(0xffD3DCDE),
+//       appBar: AppBar(
+//         elevation: 0,
+//         backgroundColor: const Color(0xffD3DCDE),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(20.0),
+//         child: Form(
+//           key: formKey,
+//           child: Center(
+//             child: SingleChildScrollView(
+//               scrollDirection: Axis.vertical,
+//               child: Column(
+//                 children: [
+//                   const Text(
+//                     'Sign in',
+//                     style:
+//                         TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+//                   ),
+//                   const SizedBox(
+//                     height: 20,
+//                   ),
+//                   const Text(
+//                     'Welcome Back',
+//                     style:
+//                         TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+//                   ),
+//                   const SizedBox(
+//                     height: 20,
+//                   ),
+//                   const Text(
+//                     'Log in with your email and your password',
+//                     style: TextStyle(
+//                         fontSize: 19.0, fontWeight: FontWeight.normal),
+//                   ),
+//                   const SizedBox(
+//                     height: 5,
+//                   ),
+//                   const Text(
+//                     'or continue with social media',
+//                     style: TextStyle(
+//                         fontSize: 19.0, fontWeight: FontWeight.normal),
+//                   ),
+//                   const SizedBox(
+//                     height: 30,
+//                   ),
+//                   defultFromFiled(
+//                       validateName: (value) {
+//                         if (value!.isEmpty) {
+//                           return 'Email empty';
+//                         }
+//                         return null;
+//                       },
+//                       label: 'email adress',
+//                       hint: 'emal',
+//                       controller: emailController,
+//                       icon: const Icon(Icons.email),
+//                       keyBoard: TextInputType.emailAddress),
+//                   const SizedBox(
+//                     height: 20,
+//                   ),
+//                   defultFromFiled(
+//                       validateName: (value) {
+//                         if (value!.isEmpty) {
+//                           return 'Password empty';
+//                         }
+//                         return null;
+//                       },
+//                       label: 'password please',
+//                       hint: 'password',
+//                       controller: passwordController,
+//                       icon: const Icon(Icons.visibility),
+//                       keyBoard: TextInputType.visiblePassword),
+//                   const SizedBox(
+//                     height: 20,
+//                   ),
+//                   defultbutton(
+//                     text: 'login',
+//                     radius: 20,
+//                     color: buttom,
+//                     onPressed:  () async {
+//                     NavigateTo(HomeLayout(), context);
+//                     if (formKey.currentState!.validate()) {
+//  // await _authhh.signInWithEmailAndPassword(email: emailController.text, password:passwordController.text);
+//                         await FirebaseAuth.instance.signInWithEmailAndPassword(email: emailController.text, password: passwordController.text);
+//  //                       await authBase.loginWithEmailAndPassword(emailController.text, passwordController.text);
+//                         // print(result);
+//
+//                        NavigateTo(HomeLayout(), context);
+//                       }
+//                     },
+//                   ),
+//                   Row(
+//                     mainAxisAlignment: MainAxisAlignment.center,
+//                     children: [
+//                       const Text('Don\'t have acount ?'),
+//                       TextButton(
+//                         onPressed: () {
+//                           NavigateTo(HotelRegisterScreen(), context);
+//                         },
+//                         child: Text(
+//                           'Register now'.toUpperCase(),
+//                           style: const TextStyle(
+//                             color: buttom,
+//                           ),
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hotel/modules/register_screen.dart';
 import 'package:hotel/shared/components/compantants.dart';
 import 'package:hotel/shared/style/colors.dart';
@@ -7,11 +147,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 import '../layout/home_layout.dart';
 
+
 class HotelLoginScreen extends StatelessWidget {
   var emailController = TextEditingController();
   var passwordController = TextEditingController();
   var formKey = GlobalKey<FormState>();
-
+  String? errorMessage;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,7 +173,7 @@ class HotelLoginScreen extends StatelessWidget {
                   const Text(
                     'Sign in',
                     style:
-                        TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+                    TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
                   ),
                   const SizedBox(
                     height: 20,
@@ -40,7 +181,7 @@ class HotelLoginScreen extends StatelessWidget {
                   const Text(
                     'Welcome Back',
                     style:
-                        TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+                    TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
                   ),
                   const SizedBox(
                     height: 20,
@@ -68,8 +209,8 @@ class HotelLoginScreen extends StatelessWidget {
                         }
                         return null;
                       },
-                      label: 'email adress',
-                      hint: 'emal',
+                      label: 'Email address',
+                      hint: 'Email',
                       controller: emailController,
                       icon: const Icon(Icons.email),
                       keyBoard: TextInputType.emailAddress),
@@ -96,19 +237,65 @@ class HotelLoginScreen extends StatelessWidget {
                     radius: 20,
                     color: buttom,
                     onPressed: () async {
-                      FirebaseAuth.instance
-                          .signInWithEmailAndPassword(
-                              email: emailController.text,
-                              password: passwordController.text)
-                          .then((value) {
-                        {
-                          NavigateTo(HomeLayout(), context);
-                        }
-                      }).onError((error, stackTrace) {
-                        print("Error ${error.toString()}");
+
+                      if (formKey.currentState!.validate())
+                      {
+                        try {
+
+                       await FirebaseAuth.instance.signInWithEmailAndPassword(email:emailController.text , password: passwordController.text)
+                          .then((value) => {
+                      Fluttertoast.showToast(msg: "Login Successful"),
+                       Navigator.pushAndRemoveUntil(
+                       context, MaterialPageRoute(builder: (context) =>  HomeLayout()),
+                       (Route<dynamic> route) => false,),
                       });
-                      if (formKey.currentState!.validate()) ;
+                      }
+                        on FirebaseAuthException catch (error) {
+                        switch (error.code) {
+                          case "invalid-email":
+                            errorMessage = "  your email address is wrong.";
+
+                            break;
+                          case "wrong-password":
+                            errorMessage = "Your password is wrong.";
+                            break;
+                          case "user-not-found":
+                            errorMessage = "User with this email doesn't exist.";
+                            break;
+                          case "user-disabled":
+                            errorMessage = "User with this email has been disabled.";
+                            break;
+                          case "too-many-requests":
+                            errorMessage = "Too many requests";
+                            break;
+                          case "operation-not-allowed":
+                            errorMessage = "Signing in with Email and Password is not enabled.";
+                            break;
+                          default:
+                            errorMessage = "An undefined Error happened.";
+                        }
+                        Fluttertoast.showToast(msg: errorMessage!);
+                        print(error.code);
+                      }
+                      }
                     },
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+
+                      TextButton(
+                        onPressed: () {
+
+                        },
+                        child: Text(
+                          'Reset password ?'.toUpperCase(),
+                          style: const TextStyle(
+                            color: buttom,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
